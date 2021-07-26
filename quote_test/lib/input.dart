dacd6a3a5ab4d2b185ca3029e089985a9aea08be
flora0110 <58814336+flora0110@users.noreply.github.com>
2021-07-27 06:03:47 +0800

    input page(not connect)

---
+++ quote_test/lib/input.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+/// This is the stateful widget that the main application instantiates.
+class Input extends StatefulWidget {
+  const Input({Key? key}) : super(key: key);
+  @override
+  State<Input> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with Input.
+class _MyStatefulWidgetState extends State<Input> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter a quote',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
